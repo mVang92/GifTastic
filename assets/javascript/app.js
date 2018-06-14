c938a7904fe03402b7167b4268d8b9dee92f598a
unknown <mVang92@github.com>
2018-06-13 23:25:49 -0500

    Updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,27 +11,32 @@ var buttons;
 
 $(document).ready(function(){
     // Initially disables the submit box
-    $("#addShow").prop("disabled", true);
+    $("#submit").prop("disabled", true);
 
     // Enables the submit button only when there is text in the dialog box
     // Diables the box if text is not present
     $("#showLength").keyup(function(){
         if($("#showLength").val() == ""){
-            $("#addShow").prop("disabled", true);
+            $("#submit").prop("disabled", true);
         } else {
-            $("#addShow").removeAttr("disabled");
+            $("#submit").removeAttr("disabled");
         }
     })
 
-    // Loop appends a button for each string in the array
-    for (i = 0; i < topics.length; i++){
-        var mButton = $("<button>")
-        mButton.addClass("gif");
-        mButton.attr("data-name", topics[i]).text(topics[i]);
-        $("#movieButton").append(" ", mButton);
+    function renderButtons(){
+        $("#movieButton").empty();
+        // Loop appends a button for each string in the array
+        for (i = 0; i < topics.length; i++){
+            var mButton = $("<button>")
+            mButton.addClass("gif");
+            mButton.attr("data-name", topics[i]);
+            mButton.text(topics[i]);
+            $("#movieButton").append(" ", mButton);
+        }
+        generateGif();
     }
 
-    $("#addShow").on("click", function(){
+    $("#submit").on("click", function(){
         // evt.preventDefault();
         var show = $("#showLength").val().trim();
         topics.push(show);
@@ -39,65 +44,60 @@ $(document).ready(function(){
         mButton.addClass("gif");
         mButton.attr("data-name", topics[i]).text(topics[i]);
         $("#movieButton").append(" ", mButton);
-
-        console.log(topics);
+        renderButtons();
+        // console.log(topics);
         $("#showLength").val("");
         $(this).prop("disabled", true);
+        
     })
 
+    function generateGif(){
     // When the user clicks on a button, the page should grab 10 static, 
     // non-animated gif images from the GIPHY API and place them on the page.
-    $(".gif").on("click", function(){
-        var show = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            show + "&api_key=whIW4NL8ItI77ZD3d2Yomtb0G40WFANS&limit=10";
+        $(".gif").on("click", function(){
+            var show = $(this).attr("data-name");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                show + "&api_key=whIW4NL8ItI77ZD3d2Yomtb0G40WFANS&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response)
-            var results = response.data;
+            // Ajax Start
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                // console.log(response)
+                var results = response.data;
 
-        // ========================
+                for (n = 0; n < results.length; n++) {
 
-        for (var i = 0; i < results.length; i++) {
+                    // Make a div with jQuery and store it in a variable named showDiv.
+                    var showDiv = $("<div>");
+                    // Make a paragraph tag with jQuery and store it in a variable named p.
+                    var p = $("<p>");
+                    // Set the inner text of the paragraph to the rating of the image in results[i].
+                    p.html(results[n].rating);
+                    // Make an image tag with jQuery and store it in a variable named image.
+                    var image = $("<img>");
+                    // Set the image's src to results[i]'s fixed_height.url.
+                    image.attr("src", results[n].images.fixed_height.url);
+                    // image.attr("src", results[n].images.fixed_height.url + "data-still=" + results[n].images.fixed_height.url + "data-animate=" + results[n].images.fixed_height.url);
+                    // Append the p variable to the showDiv variable.
+                    p.append(showDiv);
+                    // Append the image variable to the showDiv variable.
+                    showDiv.append(image);
+                    // Prepend the showDiv variable to the element with an id of gifs.
+                    $("#gifs").prepend(showDiv);    
+                }
 
-            // Make a div with jQuery and store it in a variable named movieDiv.
-            var movieDiv = $("<div>");
-            // Make a paragraph tag with jQuery and store it in a variable named p.
-            var p = $("<p>");
-            // Set the inner text of the paragraph to the rating of the image in results[i].
-            p.html(results[i].rating);
-            // Make an image tag with jQuery and store it in a variable named image.
-            var image = $("<img>");
-            // Set the image's src to results[i]'s fixed_height.url.
-            image.attr("src", results[i].images.fixed_height.url);
-            // Append the p variable to the movieDiv variable.
-            p.append(movieDiv);
-            // Append the image variable to the movieDiv variable.
-            movieDiv.append(image);
-            // Prepend the movieDiv variable to the element with an id of gifs-appear-here.
-            $("#gifs").prepend(movieDiv);
-
-            // ==================================
-            }
-
+            })
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+              }
         })
-    })
-
-    // $(".gif").on("click", function() {
-    //     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    //     var state = $(this).attr("data-state");
-    //     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    //     // Then, set the image's data-state to animate
-    //     // Else set src to the data-still value
-    //     if (state === "still") {
-    //       $(this).attr("src", $(this).attr("data-animate"));
-    //       $(this).attr("data-state", "animate");
-    //     } else {
-    //       $(this).attr("src", $(this).attr("data-still"));
-    //       $(this).attr("data-state", "still");
-    //     }
-    //   });
+    }
+    renderButtons();
 })
